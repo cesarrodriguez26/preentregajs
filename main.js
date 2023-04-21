b2b3eb3ba86791a17daaf689a0df68c975315e9e
cesarrodriguez26 <124197857+cesarrodriguez26@users.noreply.github.com>
2023-04-21 19:30:44 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,55 +1,67 @@
-console.log("Bienvenido")
+const productos = [
+{nombre: "zapatillas adidas", precio: 90},
+{nombre: "zapatillas nike", precio: 100},
+{nombre: "zapatillas puma", precio: 70},
+{nombre: "zapatillas new balance", precio: 80},
+{nombre: "zapatillas converse", precio: 60},
+];
+let carrito =[]
 
-function login(form){
-    if (form.u.value=="alberto") 
-    {
-        if (form.c.value=="123") {
-            alert("bienvenido Alberto")
-            location="principal.html";
-            
-        } else {
-                 alert("Error de password");
-        }
-        } else {
-        alert("Error de Usuario");
-    }
+let seleccion = prompt("Bienvenido a nuestra tienda!!! Desea comprar alguna zapatilla. si o no?")
 
+while(seleccion !="si" && seleccion !="no"){
+    alert("por favor ingresa si o no")
+    seleccion = prompt("hola desea comprar algo si o no")
 }
-function formula1() {
-    var CantidadT=parseInt(document.getElementById('Cant').value);
-    var Cantidad1=parseInt(document.getElementById('C1').value);
-    var Cantidad2=parseInt(document.getElementById('C2').value);
-    document.getElementById('C1').value=(Cantidad1-CantidadT);
-    document.getElementById('C2').value=(Cantidad2+CantidadT);
-    }
-function formula2() {
-        var CantidadT=parseInt(document.getElementById('Cant').value);
-        var Cantidad1=parseInt(document.getElementById('C1').value);
-        var Cantidad2=parseInt(document.getElementById('C2').value);
-        document.getElementById('C1').value=(Cantidad1+CantidadT);
-        document.getElementById('C2').value=(Cantidad2-CantidadT);
+if(seleccion == "si"){
+    alert("A continuacion nuestra lista de zapatillas")
+    let todoslosproductos = productos.map((producto)=> producto.nombre + " " + producto.precio + "$");
+    alert(todoslosproductos.join(" - "))
+}
+else if (seleccion =="no"){
+    alert("Gracias por visitar nuestra web! Hasta pronto")
+}
+while(seleccion != "no"){
+    let producto = prompt("Agrega a tu Carrito, por ejemplo zapatillas nike")
+    let precio = 0
+
+    if(producto == "zapatillas adidas" || producto == "zapatillas nike" || producto == "zapatillas puma" || producto == "zapatillas new balance" || producto == "zapatillas converse"){
+        switch(producto) {
+            case "zapatillas adidas":
+                precio =90;
+                break;
+                case "zapatillas nike":
+                precio =100;
+                break;
+                case "zapatillas puma":
+                precio =70;
+                break;
+                case "zapatillas new balance":
+                precio =80;
+                break;
+                case "zapatillas converse":
+                precio =60;
+                break;
+                default:
+                    break;
+
         }
-function formula3() {
-            var CantidadT=parseInt(document.getElementById('Cant').value);
-            var Cantidad1=parseInt(document.getElementById('C1').value);
-            var Cantidad2=parseInt(document.getElementById('C2').value);
-            document.getElementById('C1').value=(Cantidad1+CantidadT);
-            }
-function formula4() {
-                var CantidadT=parseInt(document.getElementById('Cant').value);
-                var Cantidad1=parseInt(document.getElementById('C1').value);
-                var Cantidad2=parseInt(document.getElementById('C2').value);
-                document.getElementById('C2').value=(Cantidad2+CantidadT);
-                }
-function formula5() {
-                    var CantidadT=parseInt(document.getElementById('Cant').value);
-                    var Cantidad1=parseInt(document.getElementById('C1').value);
-                    var Cantidad2=parseInt(document.getElementById('C2').value);
-                    document.getElementById('C1').value=(Cantidad1-CantidadT);
-                    }
-function formula6() {
-                        var CantidadT=parseInt(document.getElementById('Cant').value);
-                        var Cantidad1=parseInt(document.getElementById('C1').value);
-                        var Cantidad2=parseInt(document.getElementById('C2').value);
-                        document.getElementById('C2').value=(Cantidad2-CantidadT);
-                        }
+        let unidades = parseInt(prompt ("Agrega al Carrito! Ejemplo 1 2 3 4"))
+        carrito.push({producto, unidades, precio})
+        console.log(carrito)
+    }else {
+        alert("No tenemos estas zapatillas")
+    }
+    seleccion = prompt("Desea seguir comprando??")
+    while(seleccion==="no"){
+        alert("Gracias por su compra! Hasta pronto")
+        carrito.forEach((carritofinal) => {
+            console.log(`producto: ${carritofinal.producto}, unidades: ${carritofinal.unidades},
+            total a pagar por producto ${carritofinal.unidades * carritofinal.precio}`)
+
+        })
+        break;
+    }
+}
+const total = carrito.reduce((acc,el) => acc + el.precio * el.unidades, 0)
+console.log(`el total a pagar por su compra es: ${total}`)
